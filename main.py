c182047d9da22229a9cb43e9cdd925669430f0f3
islootski <islootsk@akamai.com>
2024-11-21 12:56:31 +0200

    fix github action

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from ex_proj_utils import char_utils, number_utils
 
 if __name__ == '__main__':
-    print("start")
+    print("-=-=-= Start of an OUT")
     print(char_utils.get_a(1))
     print(char_utils.get_b(2))
     print(char_utils.get_abc(3))
     print(number_utils.get_1(4))
     print(number_utils.get_2(5))
     print(number_utils.get_123(6))
-    print("end")
+    print("-=-=-= End of an OUT")
